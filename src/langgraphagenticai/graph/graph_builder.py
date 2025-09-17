c03584d74583b4eb33b05ce35a47f8173ca560d4
Bhanteja072 <bhanutejapusarla@gmail.com>
2025-09-17 21:48:42 +0530

    Connected the Pipeline to frontend

--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -20,4 +20,13 @@ class GraphBuilder:
         self.graph_builder.add_edge(START,"chatbot")
         self.graph_builder.add_edge("chatbot",END)
 
+
+    def setup_graph(self,usecase : str):
+        """
+            Sets up graph for selected usecase
+        """
+        if usecase == "Basic Chatbot":
+            self.basic_chatbot_build_graph()
+        return self.graph_builder.compile()
+
         
--- src/langgraphagenticai/main.py
+++ src/langgraphagenticai/main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from src.langgraphagenticai.ui.streamlitui.loadui import LoadStreamlitUI
+from src.langgraphagenticai.llms.groqllm import GroqLlm
+from src.langgraphagenticai.graph.graph_builder import GraphBuilder
+from src.langgraphagenticai.ui.streamlitui.display_result import DisplayResultStreamlit
 
 
 def load_langgraph_agenticai_app():
@@ -20,4 +23,29 @@ def load_langgraph_agenticai_app():
         return
 
     user_message=st.chat_input("Enter your message: ")
+    if user_message:
+        try:
+            obj_llm_config=GroqLlm(user_controls_input=user_input)
+            model=obj_llm_config.get_llm_mode()
+            if not model:
+                st.error ("Error :LLM Model could not Initilize")
+                return
+            usecase=user_input.get("selected_usecase")
+            if not usecase:
+                st.error("Error: Use Case is not fetched")
+            
+            graph_builder=GraphBuilder(model)
+            try:
+                graph=graph_builder.setup_graph(usecase)
+                DisplayResultStreamlit(usecase,graph,user_message).display_result_on_ui()
+            except Exception as e:
+                st.error(f"Error : Graph Setup Failed -{e}")
+                return
+        except Exception as e:
+                st.error(f"Error : Graph Setup Failed -{e}")
+                return
+
+
+
+
 
