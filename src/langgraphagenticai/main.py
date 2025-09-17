51d484593d664ff00b44a3baea88fee69043ded2
Bhanteja072 <bhanutejapusarla@gmail.com>
2025-09-17 12:29:30 +0530

    Built frontend

---
+++ src/langgraphagenticai/main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from src.langgraphagenticai.ui.streamlitui.loadui import LoadStreamlitUI
+
+
+def load_langgraph_agenticai_app():
+    """
+    Loads and runs the LangGraph AgenticAI application with Streamlit UI.
+    This function initializes the UI, handles user input, configures the LLM model,
+    sets up the graph based on the selected use case, and displays the output while 
+    implementing exception handling for robustness.
+
+    """
+
+    #Load Ui
+    ui=LoadStreamlitUI()
+    user_input=ui.load_streamlit_ui()
+
+    if not user_input:
+        st.error("Error: Failed to load user input from the UI.")
+        return
+
+    user_message=st.chat_input("Enter your message: ")
+
