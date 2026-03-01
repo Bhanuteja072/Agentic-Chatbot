ca06ee63181c6ad6d894ee02481950c005894fd9
Bhanteja072 <bhanutejapusarla@gmail.com>
2026-03-01 12:33:14 +0530

    Updated the prompt import

--- src/langgraphagenticai/Nodes/basic_chatbot_node.py
+++ src/langgraphagenticai/Nodes/basic_chatbot_node.py
@@ -21,7 +21,7 @@ from langchain_core.messages import HumanMessage
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from operator import itemgetter
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.messages import trim_messages
